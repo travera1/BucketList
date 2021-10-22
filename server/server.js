d5dca267bc09eb79ff9c500b4a298a95786ecea2
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-10-22 15:54:20 -0400

    scripts and setup for heroku deployment

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { PORT, mongoUri } = require('../config')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const path = require('path')
 const bucketListItemRoutes = require('./routes/api/bucketListItems')
 
 //middle ware 
@@ -22,5 +22,12 @@ mongoose
     .catch((err) => console.log(err))
 
 app.use('/api/bucketListItems', bucketListItemRoutes)
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/dist'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => console.log(`app listening at http:localhost:${PORT}`))
